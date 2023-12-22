ad24889d0f72300d81d18a017369df406c42c80c
josephtamrat123 <141209846+josephtamrat123@users.noreply.github.com>
2023-12-23 00:00:06 +0300

    Update 0-main.c

--- 0x1A-hash_tables/mains/0-main.c
+++ 0x1A-hash_tables/mains/0-main.c
@@ -1,18 +1,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include "hash_tables.h"
+#include "../hash_tables.h"
 
 /**
- * main - check the code for
- *
- * Return: Always EXIT_SUCCESS.
+ * main -this  checks the code for Holberton School students.
+ *return:this  always returns  EXIT_SUCCESS.
  */
 int main(void)
 {
-    hash_table_t *ht;
+	hash_table_t *ht;
 
-    ht = hash_table_create(1024);
-    printf("%p\n", (void *)ht);
-    return (EXIT_SUCCESS);
+	ht = hash_table_create(1024);
+	printf("%p\n", (void *)ht);
+	return (EXIT_SUCCESS);
 }
